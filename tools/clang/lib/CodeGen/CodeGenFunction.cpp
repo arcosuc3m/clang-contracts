arcosuc3m/clang-contracts

commit 8d61515dad06933cca0cdc9a64f55be62a33ca6e

Closes issue #14.
- CodeGenFunction.cpp: FunctionDecl copy now has template information.
- {Itanium,Microsoft}Mangle.cpp: unchecked functions mangling

tools/clang/lib/CodeGen/CodeGenFunction.cpp
@@ -1288,9 +1288,39 @@ SynthesizeCheckedFunctionBody(CodeGenModule &CGM, FunctionDecl *D_body, Function
   return new (Context) CompoundStmt(Context, S, ISL, ISL);
 }
 
+/// \brief Make a (partial) copy of the original CXXMethodDecl/FunctionDecl. For
+///  CXXMethodDecl, don't care if it is a CXX{Constructor,Destructor}Decl.
+static FunctionDecl *
+PartialCopyFunctionDecl(ASTContext &Context, FunctionDecl *FD) {
+  FunctionDecl *ret;
+  if (isa<CXXMethodDecl>(FD)) {
+    ret = CXXMethodDecl::Create(Context, cast<CXXRecordDecl>(FD->getDeclContext()),
+				FD->getLocStart(), FD->getNameInfo(),
+				FD->getType(), FD->getTypeSourceInfo(),
+				FD->getStorageClass(), FD->isInlineSpecified(),
+				FD->isConstexpr(), FD->getLocEnd());
+  } else {
+    ret = FunctionDecl::Create(Context, FD->getDeclContext(), FD->getLocStart(),
+			       FD->getNameInfo(), FD->getType(), FD->getTypeSourceInfo(),
+			       FD->getStorageClass(), FD->isInlineSpecified(),
+			       FD->hasWrittenPrototype(), FD->isConstexpr());
+  }
+  ret->setImplicit();
+
+  ret->setParams(FD->parameters());
+  ret->setBody(FD->getBody());
+  if (FD->getPrimaryTemplate())
+    ret->setFunctionTemplateSpecialization(FD->getPrimaryTemplate(),
+					   FD->getTemplateSpecializationArgs(),
+					   nullptr);
+  ret->setAccess(FD->getAccess());
+
+  return ret;
+}
+
 void CodeGenFunction::GenerateCode(GlobalDecl GD, llvm::Function *Fn,
                                    const CGFunctionInfo &FnInfo) {
-  const FunctionDecl *FD = cast<FunctionDecl>(GD.getDecl());
+  FunctionDecl *FD = const_cast<FunctionDecl *>(cast<FunctionDecl>(GD.getDecl()));
   CurGD = GD;
 
   FunctionArgList Args;
@@ -1298,51 +1328,38 @@ void CodeGenFunction::GenerateCode(GlobalDecl GD, llvm::Function *Fn,
 
   if (getLangOpts().BuildLevel > 0 // off
       && (FD->hasAttr<ExpectsAttr>() || FD->hasAttr<EnsuresAttr>())) {
-    IdentifierInfo *II = &getContext().Idents.get(CXX__UNCHK_FN_PREFIX
-                             + FD->getNameAsString());
-    FunctionDecl *unchk_FD, *_D = const_cast<FunctionDecl *>(FD);
+    // Make a partial copy (unchk_FD) of the original (FD) function; the copy
+    // has the `P0542R5_Unchecked' bit set, which alters name mangling -see
+    // tools/clang/lib/AST/{Itanium,Microsoft}Mangle.cpp. This symbol is usually
+    // private and is always_inline as part of FD.
+    //
+    //                                      (1)    ,----------.  EmitGlobal()
+    //                                          ,--| unchk_FD |-------------->
+    //                                         ,   `----------'
+    // ,-----------------.                    ,  
+    // | FD (has expects | GenerateCode()    ,     ,----.
+    // |    /ensures)    |--------------->--+--->--| FD |-------->
+    // `-----------------'                    (2)  `----'
+    //                                        Here, FD body has been replaced
+    //                                        by {pre,post}condition checks
+    //                                        and a call to unchk_FD, which is
+    //                                        always_inline. 
+    auto unchk_FD = PartialCopyFunctionDecl(getContext(), FD);
 
-    // Make a (partial) copy of the original CXXMethodDecl/FunctionDecl, but add
-    // the CXX__UNCHK_FN_PREFIX suffix to the name.
-    // For CXXMethodDecl, don't care if it is a CXX{Constructor,Destructor}Decl.
-    if (isa<CXXMethodDecl>(_D)) {
-      unchk_FD = CXXMethodDecl::Create(getContext(),
-                    cast<CXXRecordDecl>(_D->getDeclContext()), _D->getLocStart(),
-                    DeclarationNameInfo(DeclarationName(II), _D->getNameInfo().getLoc()),
-                    _D->getType(), _D->getTypeSourceInfo(), _D->getStorageClass(),
-                    _D->isInlineSpecified(), _D->isConstexpr(), _D->getLocEnd());
-    } else {
-      unchk_FD = FunctionDecl::Create(getContext(),
-                    _D->getDeclContext(), _D->getLocStart(), _D->getNameInfo().getLoc(),
-                    DeclarationName(II), _D->getType(), _D->getTypeSourceInfo(),
-                    _D->getStorageClass(), _D->isInlineSpecified(),
-                    _D->hasWrittenPrototype(), _D->isConstexpr());
-    }
-    unchk_FD->setImplicit();
-    unchk_FD->setParams(_D->parameters());
-    unchk_FD->setAccess(_D->getAccess());
-    unchk_FD->setBody(_D->getBody());
-    _D->getParent()->addDecl(unchk_FD);
-
-    // Emit the '__unchk_' function
+    //FD->getParent()->addDecl(unchk_FD);
+    unchk_FD->setP0542R5_Unchecked();
     CGM.EmitGlobal(unchk_FD);
 
-    // Replaces the body of the original (not yet emitted) function
-    _D->setBody(SynthesizeCheckedFunctionBody(CGM, unchk_FD, _D));
-    _D->dropAttr<ExpectsAttr>();
-    _D->dropAttr<EnsuresAttr>();
+    // (2) Replaces the body of FD
+    FD->setBody(SynthesizeCheckedFunctionBody(CGM, unchk_FD, FD));
+    FD->dropAttr<ExpectsAttr>(), FD->dropAttr<EnsuresAttr>();
   }
 
-  // Sets the AlwaysInline attribute for '__unchk_' functions, so that they
+  // Sets the AlwaysInline attribute for unchecked functions, so that they
   // are inlined as part of the function that checks pre/post-conditions.
-  //
-  // Because it is always inlined, InternalLinkage causes the function to be removed
-  // from the IR.
-  if (FD->isImplicit()
-      && StringRef(FD->getNameAsString()).startswith(CXX__UNCHK_FN_PREFIX)) {
-    Fn->addFnAttr(llvm::Attribute::AlwaysInline);
-    Fn->setLinkage(llvm::Function::InternalLinkage);
-  }
+  // Additionally, InternalLinkage causes the function to be hidden from the IR.
+  if (FD->isP0542R5_Unchecked())
+    Fn->addFnAttr(llvm::Attribute::AlwaysInline), Fn->setLinkage(llvm::Function::InternalLinkage);
 
   // Check if we should generate debug info for this function.
   if (FD->hasAttr<NoDebugAttr>())

tools/clang/include/clang/AST/Decl.h
@@ -1668,7 +1668,7 @@ private:
   unsigned IsLateTemplateParsed : 1;
   unsigned IsConstexpr : 1;
   unsigned InstantiationIsPending:1;
-  VarDecl *________ret________;
+  unsigned P0542R5_Unchecked : 1;
 
   /// \brief Indicates if the function uses __try.
   unsigned UsesSEHTry : 1;
@@ -1711,6 +1711,9 @@ private:
   /// DNLoc - Provides source/type location info for the
   /// declaration name embedded in the DeclaratorDecl base class.
   DeclarationNameLoc DNLoc;
+
+  /// \brief P0542R5: a local that temporarily stores return value [internal, cached].
+  VarDecl *________ret________;
 
   /// \brief Specify that this function declaration is actually a function
   /// template specialization.
@@ -1763,10 +1766,10 @@ protected:
         IsDeleted(false), IsTrivial(false), IsDefaulted(false),
         IsExplicitlyDefaulted(false), HasImplicitReturnZero(false),
         IsLateTemplateParsed(false), IsConstexpr(isConstexprSpecified),
-        InstantiationIsPending(false), ________ret________(nullptr),
+        InstantiationIsPending(false), P0542R5_Unchecked(false),
         UsesSEHTry(false), HasSkippedBody(false), WillHaveBody(false),
         EndRangeLoc(NameInfo.getEndLoc()), TemplateOrSpecialization(),
-        DNLoc(NameInfo.getInfo()) {}
+        DNLoc(NameInfo.getInfo()), ________ret________(nullptr) {}
 
   typedef Redeclarable<FunctionDecl> redeclarable_base;
   FunctionDecl *getNextRedeclarationImpl() override {
@@ -1972,6 +1975,11 @@ public:
   /// assumption that the instantiation will happen in some other TU.
   bool instantiationIsPending() const { return InstantiationIsPending; }
   void setInstantiationIsPending(bool IC) { InstantiationIsPending = IC; }
+
+  /// \brief Whether this function is the unchecked equivalent of a FunctionDecl
+  ///  that has {Expects,Ensures}Attr.
+  bool isP0542R5_Unchecked() const { return P0542R5_Unchecked; }
+  void setP0542R5_Unchecked(bool U = true) { P0542R5_Unchecked = U; }
 
   /// \brief Indicates the function uses __try.
   bool usesSEHTry() const { return UsesSEHTry; }

tools/clang/lib/AST/ItaniumMangle.cpp
@@ -723,6 +723,10 @@ void CXXNameMangler::mangleFunctionEncodingBareType(const FunctionDecl *FD) {
     FunctionTypeDepth.pop(Saved);
   }
 
+  // P0542R5: vendor extended type qualifier used for unchecked functions.
+  if (FD->isP0542R5_Unchecked())
+    Out << "U9unchecked";
+
   // When mangling an inheriting constructor, the bare function type used is
   // that of the inherited constructor.
   if (auto *CD = dyn_cast<CXXConstructorDecl>(FD))

tools/clang/lib/AST/MicrosoftMangle.cpp
@@ -467,6 +467,11 @@ void MicrosoftCXXNameMangler::mangleFunctionEncoding(const FunctionDecl *FD,
     if (FD->isExternC() && FD->hasAttr<OverloadableAttr>())
       Out << "$$J0";
 
+    // P0542R5: for unchecked functions.  This is not compliant with MSVC++ name
+    // mangling scheme.  Anyway, these symbols are not public.
+    if (FD->isP0542R5_Unchecked())
+      Out << "$U";
+
     mangleFunctionClass(FD);
 
     mangleFunctionType(FT, FD);

tools/clang/lib/CodeGen/CGStmt.cpp
@@ -579,14 +579,10 @@ void CodeGenFunction::EmitAssertAttr(const AssertAttr *_Attr,
                                           /*Offset=*/0, SM, CGM.getLangOpts()));
     StringRef __comment(S, E-S);
 
-    // __func is the function name as it appears on the source code (if needed, remove '__unchk_' prefix)
-    StringRef __func{cast<FunctionDecl>(CurFuncDecl)->getNameAsString()};
-    if (CurFuncDecl->isImplicit())
-      __func.consume_front(CXX__UNCHK_FN_PREFIX);
-
     // Register a std::contract_violation object in `__contract_violation_tab[]'
     llvm::APInt I = CGM.Register_contract_violation(_Attr->getLocation(),
-                                                    __func.str(), __comment, Level);
+                                                    cast<FunctionDecl>(CurFuncDecl)->getNameAsString(),
+						    __comment, Level);
     IntegerLiteral *IL = IntegerLiteral::Create(C, I, C.IntTy, SourceLocation());
 
     // Add argument of type `const __builtin_contract_violation&' to the Args vector

tools/clang/lib/CodeGen/CodeGenFunction.h
@@ -1623,9 +1623,6 @@ public:
 
   QualType BuildFunctionArgList(GlobalDecl GD, FunctionArgList &Args);
 
-  // prefix used for unchecked functions (C++ contracts); see CodeGenFunction::GenerateCode()
-  static constexpr const char *CXX__UNCHK_FN_PREFIX = "__unchk_";
-  
   void GenerateCode(GlobalDecl GD, llvm::Function *Fn,
                     const CGFunctionInfo &FnInfo);
   /// \brief Emit code for the start of a function.

commit d7832a7fb62151cd3b2ddfd3e371d3e2a9595bee

Closes issue #17 (regression introduced in commit 8d61515d).

tools/clang/lib/CodeGen/CodeGenFunction.cpp
@@ -1292,21 +1292,29 @@ SynthesizeCheckedFunctionBody(CodeGenModule &CGM, FunctionDecl *D_body, Function
 ///  CXXMethodDecl, don't care if it is a CXX{Constructor,Destructor}Decl.
 static FunctionDecl *
 PartialCopyFunctionDecl(ASTContext &Context, FunctionDecl *FD) {
+  // If this name is a simple identifier, reuse the DeclarationName. Otherwise,
+  // (for a constructor, destructor, or conversion function) construct a non-
+  // special name (required).
+  auto DN_FD = FD->getDeclName();
+  auto DN_ret = DN_FD.isIdentifier() ? DN_FD
+    : DeclarationName{&Context.Idents.get(DN_FD.getAsString())};
+  DeclarationNameInfo NI_ret{DN_ret, SourceLocation{}};
+
   FunctionDecl *ret;
   if (isa<CXXMethodDecl>(FD)) {
     ret = CXXMethodDecl::Create(Context, cast<CXXRecordDecl>(FD->getDeclContext()),
-				FD->getLocStart(), FD->getNameInfo(),
-				FD->getType(), FD->getTypeSourceInfo(),
-				FD->getStorageClass(), FD->isInlineSpecified(),
-				FD->isConstexpr(), FD->getLocEnd());
+				SourceLocation{}, NI_ret, FD->getType(),
+				FD->getTypeSourceInfo(), FD->getStorageClass(),
+				FD->isInlineSpecified(), FD->isConstexpr(),
+				SourceLocation{});
   } else {
-    ret = FunctionDecl::Create(Context, FD->getDeclContext(), FD->getLocStart(),
-			       FD->getNameInfo(), FD->getType(), FD->getTypeSourceInfo(),
+    ret = FunctionDecl::Create(Context, FD->getDeclContext(), SourceLocation{},
+			       NI_ret, FD->getType(), FD->getTypeSourceInfo(),
 			       FD->getStorageClass(), FD->isInlineSpecified(),
 			       FD->hasWrittenPrototype(), FD->isConstexpr());
   }
-  ret->setImplicit();
 
+  ret->setImplicit();
   ret->setParams(FD->parameters());
   ret->setBody(FD->getBody());
   if (FD->getPrimaryTemplate())